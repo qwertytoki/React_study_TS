d6337451bd9285191281afa2e5f37abdefb14889
qwertytoki <qwertytoki@gmail.com>
2020-05-26 00:32:32 +0800

    async sample

--- small_sample/sample.js
+++ small_sample/sample.js
@@ -1,3 +1,5 @@
+// spread property
+
 console.log("hello world");
 const arr1 = ["A", "B", "C"];
 const arr2 = [...arr1, "D", "E", "D", "E", "D", "E"];
@@ -7,3 +9,34 @@ console.log(arr3);
 constobj1 = { a: 1, b: 2, c: 3 };
 const obj2 = { ...constobj1, d: 4, e: 5 };
 console.log(obj2); // { a: 1, b: 2, c: 3, d: 4, e: 5 }
+
+// short hand of property
+const foo = 644459;
+const obj = { foo, bar: 45 };
+console.log(obj);
+
+// async
+// const wakeup = (ms) => {
+//   setTimeout(() => {
+//     console.log("woke up!");
+//   }, ms);
+// };
+// const greet = () => {
+//   console.log("good night!");
+//   wakeup(2000);
+//   console.log("good morning!");
+// };
+// greet();
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const greet = async () => {
+  console.log("お や す み ");
+  try {
+    await sleep(2000);
+    console.log("起 き た ");
+    console.log("お は よ う ! ");
+  } catch (err) {
+    console.error("睡 眠 例 外 で す : ", err);
+  }
+};
+greet();
