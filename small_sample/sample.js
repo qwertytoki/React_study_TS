c4195fece53f877db5a061e9d35bb2bae85b80a1
qwertytoki <qwertytoki@gmail.com>
2020-05-26 00:09:19 +0800

    spread structure

--- small_sample/sample.js
+++ small_sample/sample.js
@@ -1,1 +1,7 @@
 console.log("hello world");
+const arr1 = ["A", "B", "C"];
+const arr2 = [...arr1, "D", "E"];
+console.log(arr2); // [ 'A', 'B', 'C', 'D', 'E', 'F' ]
+constobj1 = { a: 1, b: 2, c: 3 };
+const obj2 = { ...constobj1, d: 4, e: 5 };
+console.log(obj2); // { a: 1, b: 2, c: 3, d: 4, e: 5 }
